fd302a27643c31eef08d1323d5bf44532bbfbd76
Alexander Kühn <prj@alexkuehn.de>
2024-10-18 16:27:30 +0200

    fixed some issues

--- private_dot_emacs.d/customizations.el
+++ private_dot_emacs.d/customizations.el
@@ -3,8 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tango-dark))
- '(package-selected-packages '(use-package))
+ '(custom-enabled-themes '(monokai))
+ '(custom-safe-themes
+   '("8dbbcb2b7ea7e7466ef575b60a92078359ac260c91fe908685b3983ab8e20e3f" default))
+ '(ispell-dictionary nil)
+ '(package-selected-packages '(monokai-theme use-package))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -10,20 +10,7 @@
 (prefer-coding-system 'utf-8)
 
 (setq initial-major-mode 'fundamental-mode)  ; default mode for the *scratch* buffer
-(setq display-time-default-load-average nil) ; this information is useless for most
 
-
-;; Automatically reread from disk if the underlying file changes
-(setq auto-revert-avoid-polling t)
-
-(setq auto-revert-interval 5)
-(setq auto-revert-check-vc-info t)
-(global-auto-revert-mode)
-;; Save history of minibuffer
-(savehist-mode)
-
-;; Move through windows with Ctrl-<arrow keys>
-(windmove-default-keybindings 'control) 
 
 ;; Fix archaic defaults
 (setq sentence-end-double-space nil)
@@ -42,7 +29,6 @@ If the new path's directories does not exist, create them."
 (setq make-backup-file-name-function 'config--backup-file-name)
 
 
-
 ;; add package repositories and initialize packaging system
 (with-eval-after-load 'package
   (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
@@ -54,22 +40,11 @@ If the new path's directories does not exist, create them."
 (require 'use-package)
 
 
-;; Show the help buffer after startup
-;(add-hook 'after-init-hook 'help-quick)
-
-;; which-key: shows a popup of available keybindings when typing a long key
-;; sequence (e.g. C-x ...)
-(use-package which-key
-  :ensure t
-  :config
-  (which-key-mode))
-
-
 (setq line-number-mode t)                        ; Show current line in modeline
 (setq column-number-mode t)                      ; Show column as well
 
 (setq x-underline-at-descent-line nil)           ; Prettier underlines
-(setq switch-to-buffer-obey-display-actions t)   ; Make switching buffers more consistent
+
 
 (setq show-trailing-whitespace nil)      ; By default, don't underline trailing spaces
 (setq indicate-buffer-boundaries 'left)  ; Show buffer top and bottom in the margin
@@ -85,8 +60,7 @@ If the new path's directories does not exist, create them."
 (setq tab-width 4)
 
 (setq ring-bell-function 'ignore)
-(setq load-prefer-newer t)
-(setq read-process-output-max (* 1024 1024))
+
 
 ;; Display line numbers in programming mode
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
@@ -95,8 +69,8 @@ If the new path's directories does not exist, create them."
 (add-hook 'text-mode-hook 'visual-line-mode)
 
 ;; Modes to highlight the current line with
-(let ((hl-line-hooks '(text-mode-hook prog-mode-hook)))
-  (mapc (lambda (hook) (add-hook hook 'hl-line-mode)) hl-line-hooks))
+;;(let ((hl-line-hooks '(text-mode-hook prog-mode-hook)))
+;;  (mapc (lambda (hook) (add-hook hook 'hl-line-mode)) hl-line-hooks))
 
 ;; respect customize files
 (setq custom-file (concat user-emacs-directory "customizations.el"))

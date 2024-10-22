67ed55079214300aeac1983f92f9d306bfb9f332
Alexander Kühn <prj@alexkuehn.de>
2024-10-22 14:20:25 +0200

    emacs: fix error when started in terminal

--- private_dot_emacs.d/early-init.el
+++ private_dot_emacs.d/early-init.el
@@ -8,15 +8,12 @@
 ;; settings for better startup performance
 (setq orig-initial-gc-threshold gc-cons-threshold)
 (setq gc-cons-threshold 10000000)
-(setq byte-compile-warnings '(not obsolete))
-(setq warning-suppress-log-types '((comp) (bytecomp)))
-(setq native-comp-async-report-warnings-errors 'silent)
+
 
 ;; silence startup message
-(setq inhibit-startup-echo-area-message (user-login-name))
-
 
 ;; Disable GUI
-(tool-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
 
 ;;; early-init.el ends here
